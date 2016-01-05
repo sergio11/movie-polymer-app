00ac31c00d432a4e52b276e234821f07fd3f4fc3
Sergio <sss4esob@gmail.com>
2016-01-05 14:57:40 +0100

    Aplicación de mixins para paper-card y creación del gulpfile para automaticación del despliegue.

---
+++ gulpfile.js
@@ -0,0 +1,169 @@
+'use strict';
+
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var htmlAutoprefixer = require( "gulp-html-autoprefixer" );
+var path = require('path');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var historyApiFallback = require('connect-history-api-fallback');
+var ifElse = require('gulp-if-else');
+
+var AUTOPREFIXER_BROWSERS = [
+  'ie >= 10',
+  'ie_mob >= 10',
+  'ff >= 30',
+  'chrome >= 34',
+  'safari >= 7',
+  'opera >= 23',
+  'ios >= 7',
+  'android >= 4.4',
+  'bb >= 10'
+];
+
+var DIST = 'dist';
+
+var dist = function(subpath) {
+  return !subpath ? DIST : path.join(DIST, subpath);
+};
+
+//Tarea para manejo de hojas de estilos
+var styleTask = function(stylesPath, srcs) {
+  return gulp.src(srcs.map(function(src) {
+      return path.join('app', stylesPath, src);
+    }))
+  	//Los que han cambiado.
+    .pipe($.changed(stylesPath))
+    .pipe(ifElse(false,function(){
+    	return $.autoprefixer(AUTOPREFIXER_BROWSERS);
+    },htmlAutoprefixer))
+    .pipe(gulp.dest('.tmp/' + stylesPath))
+    .pipe(ifElse(false,$.minifyCss,function(){
+    	return $.minifyHtml({
+	      quotes: true,
+	      empty: true,
+	      spare: true
+	    });
+    }))
+    .pipe(gulp.dest(dist(stylesPath)))
+    .pipe($.size({title: stylesPath}));
+};
+
+//Tarea para optimización de imagenes.
+var imageOptimizeTask = function(src, dest) {
+  return gulp.src(src)
+    .pipe($.imagemin({
+      progressive: true,
+      interlaced: true
+    }))
+    .pipe(gulp.dest(dest))
+    .pipe($.size({title: 'images'}));
+};
+//Optimiza HTML.
+var optimizeHtmlTask = function(src, dest) {
+  //Obtiene los assets.	
+  var assets = $.useref.assets({
+    searchPath: ['.tmp', 'app']
+  });
+
+  return gulp.src(src)
+    .pipe(assets)
+    // Concatenate and minify JavaScript
+    .pipe($.if('*.js', $.uglify({
+      preserveComments: 'some'
+    })))
+    // Concatenate and minify styles
+    // In case you are still using useref build blocks
+    .pipe($.if('*.css', $.minifyCss()))
+    .pipe(assets.restore())
+    .pipe($.useref())
+    // Minify any HTML
+    .pipe($.if('*.html', $.minifyHtml({
+      quotes: true,
+      empty: true,
+      spare: true
+    })))
+    // Output files
+    .pipe(gulp.dest(dest))
+    .pipe($.size({
+      title: 'html'
+    }));
+};
+
+// Compile and automatically prefix stylesheets
+gulp.task('styles', function() {
+  return styleTask('styles', ['**/*']);
+});
+
+gulp.task('elements', function() {
+  return styleTask('elements', ['**/*.css']);
+});
+
+// Optimize images
+gulp.task('images', function() {
+  return imageOptimizeTask('app/images/**/*', dist('images'));
+});
+
+// Copy web fonts to dist
+gulp.task('fonts', function() {
+  return gulp.src(['app/fonts/**'])
+    .pipe(gulp.dest(dist('fonts')))
+    .pipe($.size({
+      title: 'fonts'
+    }));
+});
+
+// Scan your HTML for assets & optimize them
+gulp.task('html', function() {
+  return optimizeHtmlTask(
+    ['app/**/*.html', '!app/{elements,test,bower_components}/**/*.html'],
+    dist());
+});
+
+// Vulcanize granular configuration
+gulp.task('vulcanize', function() {
+  return gulp.src('app/elements/elements.html')
+    .pipe($.vulcanize({
+      stripComments: true,
+      inlineCss: true,
+      inlineScripts: true
+    }))
+    .pipe(gulp.dest(dist('elements')))
+    .pipe($.size({title: 'vulcanize'}));
+});
+
+// Clean output directory
+gulp.task('clean', function() {
+  return del(['.tmp', dist()]);
+});
+
+// Watch files for changes & reload
+gulp.task('serve', ['styles', 'elements', 'images'], function() {
+  browserSync({
+    port: 5000,
+    notify: false,
+    logPrefix: 'PSK',
+    snippetOptions: {
+      rule: {
+        match: '<span id="browser-sync-binding"></span>',
+        fn: function(snippet) {
+          return snippet;
+        }
+      }
+    },
+    // Run as an https by uncommenting 'https: true'
+    // Note: this uses an unsigned certificate which on first access
+    //       will present a certificate warning in the browser.
+    // https: true,
+    server: {
+      baseDir: ['.tmp', 'app'],
+      middleware: [historyApiFallback()]
+    }
+  });
+
+  gulp.watch(['app/**/*.html'], reload);
+  gulp.watch(['app/styles/**/*.css'], ['styles', reload]);
+  gulp.watch(['app/elements/**/*.css'], ['elements', reload]);
+  gulp.watch(['app/images/**/*'], reload);
+
+});
